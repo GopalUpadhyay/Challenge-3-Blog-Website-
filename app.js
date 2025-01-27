8a1e236c1c054ecd9e350d2241ef6bf62fa5fe19
Gopal Upadhyay <gopalupadhyay2345@gmail.com>
2025-01-27 18:25:58 +0530

    Fourth Commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,41 +9,38 @@ app.set('view engine', 'ejs');
 let shorter = {Home : 'This Is The First Line Of This Blog WebeSite.'};
 let fullData = {Home : 'This Is The First Line Of This Blog WebeSite.'};
 
-app.get('/', (req, res) => {    
-    res.render('home', { output: temp , title : title, check : output});
+app.get('/', (req, res) => {
+    res.render('home', {output : shorter, check : fullData, title: Object.keys(shorter)});
 });
 
 
-let temp = [];
+let temp = ['This Is The First Line Of This Blog WebeSite.'];
 app.post('/input', (req, res) => {
 
     // Practice START.
     let text = req.body.output;
 
-    if(text.length > 100){
-        text = text.substr(0,100) + ('...');
-        temp.push(text);
-        title.push(req.body.title);
-        output.push(req.body.output);
+    if (text.length > 100) {
+        text = text.substr(0, 100) + ('...');
+        shorter[req.body.title] = text;
+        fullData[req.body.title] = req.body.output;
         res.redirect('/');
     }
     // Practice END.
-    else{
-        temp.push(text);
-        title.push(req.body.title);
-        output.push(req.body.output);
+    else {
+        shorter[req.body.title] = text;
+        fullData[req.body.title] = req.body.output;
         res.redirect('/');
     }
 })
 
-
-app.get('/ReadMore', (req, res)=>{
-    res.render('ReadMore', {output : output, title : title});
-})
+app.get('/ReadMore/:value', (req, res) => {
+    res.render('ReadMore', {data : fullData, title: req.params.value});
+});
 
 app.get('/input', (req, res) => {
     res.render('input');
-})
+});
 
 let About = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam est tenetur quas quasi aperiam sit assumenda laboriosam ab, quidem odit dolore inventore praesentium eveniet, quod, pariatur tempore! Voluptas, fugit ea.';
 app.get('/about', (req, res) => {
